1975e26aa247a7c7a9ae525f7074dea9b1c78475
AkshitaPatel <akshitapatel02@gmail.com>
2023-01-08 20:57:38 -0800

    Adding loading spinner & variable pageSize to NewsMonkey

--- src/components/News.js
+++ src/components/News.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
 
 export class News extends Component {
-  constructor() {
+  constructor(props) {
     super();
     this.state = {
       articles: [],
@@ -12,13 +13,16 @@ export class News extends Component {
   }
 
   async componentDidMount() {
-    let url =
-      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4ca8050060df40a7b91172d6dce49e72&page=1&pageSize=20";
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4ca8050060df40a7b91172d6dce49e72&page=1&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
     let data = await fetch(url);
     let parsedData = await data.json(data);
     this.setState({
       articles: parsedData.articles,
       totalResults: parsedData.totalResults,
+      loading: false,
     });
   }
 
@@ -27,48 +31,66 @@ export class News extends Component {
     } else {
       let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4ca8050060df40a7b91172d6dce49e72&page=${
         this.state.page + 1
-      }&pageSize=20`;
+      }&pageSize=${this.props.pageSize}`;
+      this.setState({
+        loading: true,
+      });
       let data = await fetch(url);
       let parsedData = await data.json(data);
-      this.setState({ articles: parsedData.articles });
-      this.setState({ page: this.state.page + 1 });
+
+      this.setState({
+        articles: parsedData.articles,
+        page: this.state.page + 1,
+        loading: false,
+      });
     }
   };
 
   handlePrevClick = async () => {
     let url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=4ca8050060df40a7b91172d6dce49e72&page=${
       this.state.page - 1
-    }&pageSize=20`;
+    }&pageSize=${this.props.pageSize}`;
+    this.setState({
+      loading: true,
+    });
     let data = await fetch(url);
     let parsedData = await data.json(data);
-    this.setState({ articles: parsedData.articles });
-    this.setState({ page: this.state.page - 1 });
+
+    this.setState({
+      articles: parsedData.articles,
+      page: this.state.page - 1,
+      loading: false,
+    });
   };
 
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h2 className="text-center">NewsMonkey - Top Headlines</h2>
+        {this.state.loading && <Spinner />}
 
         <div className="row">
-          {this.state.articles.map((element) => {
-            return (
-              <div className="col-md-4" key={element.url}>
-                <NewsItem
-                  title={!element.title ? "" : element.title.slice(0, 45)}
-                  description={
-                    !element.description ? "" : element.description.slice(0, 88)
-                  }
-                  imageUrl={
-                    element.urlToImage
-                      ? element.urlToImage
-                      : "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/12/0/0/TESLA-PRICES-e1672173644348.jpg?ve=1&tl=1"
-                  }
-                  newsUrl={element.url}
-                ></NewsItem>
-              </div>
-            );
-          })}
+          {!this.state.loading &&
+            this.state.articles.map((element) => {
+              return (
+                <div className="col-md-4" key={element.url}>
+                  <NewsItem
+                    title={!element.title ? "" : element.title.slice(0, 45)}
+                    description={
+                      !element.description
+                        ? ""
+                        : element.description.slice(0, 88)
+                    }
+                    imageUrl={
+                      element.urlToImage
+                        ? element.urlToImage
+                        : "https://a57.foxnews.com/static.foxbusiness.com/foxbusiness.com/content/uploads/2022/12/0/0/TESLA-PRICES-e1672173644348.jpg?ve=1&tl=1"
+                    }
+                    newsUrl={element.url}
+                  ></NewsItem>
+                </div>
+              );
+            })}
         </div>
         <div className="container d-flex justify-content-between">
           <button
@@ -81,7 +103,10 @@ export class News extends Component {
           </button>
           <button
             type="button"
-            // disabled={}
+            disabled={Math.ceil(
+              this.state.page + 1 >
+                this.state.totalResults / this.props.pageSize
+            )}
             className="btn btn-dark"
             onClick={this.handleNextClick}
           >
